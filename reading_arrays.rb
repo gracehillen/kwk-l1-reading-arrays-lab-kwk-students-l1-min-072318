16e9dcb0354cdcead92d44135a452558039a9669
gracehillen <grahillen19@ahastars.org>
2018-07-25 14:56:39 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,15 +8,15 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-   Write a solution that returns the first student in the array student_names#
+  puts room.first
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts room[3]
 end
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts
 end
 
 def first_student_by_method
