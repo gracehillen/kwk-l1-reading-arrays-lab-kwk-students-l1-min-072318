16a5fd049e2e6ede2492b1aff9a663ddaff1fa0b
gracehillen <grahillen19@ahastars.org>
2018-07-25 15:02:27 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,25 +8,25 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  puts STUDENT_NAMES[1]
+  return STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+  return STUDENT_NAMES[3]
 end
 
 def last_student_by_index
-  puts STUDENT_NAMES[6]
+  return STUDENT_NAMES[6]
 end
 
 def first_student_by_method
-  puts STUDENT_NAMES.first
+  return STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  puts STUDENT_NAMES.last
+  return STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-  puts STUDENT_NAMES[0..2]
+  return STUDENT_NAMES[0..2]
 end
